e049733848f6f4df0f35364e910fd87e57685ed2
isNastish <paukden12@gmail.com>
2022-05-01 19:02:57 +0300

    Test commit.

--- code/w32_main.c
+++ code/w32_main.c
@@ -1,11 +1,45 @@
 
 
-
 #include <windows.h>
+
+#define global static
+#define internal static
+#define local_persist static
+
+global HINSTANCE global_instance_handle;
+
+LRESULT w32_window_proc(HWND window,
+                        UINT msg,
+                        WPARAM w_param,
+                        LPARAM l_param){
+    LRESULT result = 0;
+    
+    switch(msg){
+        default:{
+            result = DefWindowProc(window, msg, w_param, l_param);
+        }break;
+    }
+    
+    return(result);
+}
+
 
 INT WINAPI WinMain(HINSTANCE instance,
                    HINSTANCE prev_instance,
                    PSTR cmd_line,
                    int show_code){
+
+    global_instance_handle = instance;
+    
+    WNDCLASSA window_class = {0};
+    {
+        window_class.style = CS_HREDRAW | CS_VREDRAW;
+        window_class.lpfnWndProc = w32_window_proc;
+        window_class.hInstance = global_instance_handle;
+        //window_class.hIcon = ;
+        //window_class.hCursor = ;
+        window_class.lpszClassName = "AppWindowClass";
+    }
+    
     return(0);
 }
