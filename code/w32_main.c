9831a40574e8420b9f4d24ff785db8579959cde2
isNastish <paukden12@gmail.com>
2022-05-01 19:53:05 +0300

    Window creation.

--- code/w32_main.c
+++ code/w32_main.c
@@ -1,11 +1,23 @@
 
 
 #include <windows.h>
+#include <stdint.h>
 
 #define global static
 #define internal static
 #define local_persist static
 
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+global s8 global_running;
 global HINSTANCE global_instance_handle;
 
 LRESULT w32_window_proc(HWND window,
@@ -15,6 +27,11 @@ LRESULT w32_window_proc(HWND window,
     LRESULT result = 0;
     
     switch(msg){
+        case(WM_CLOSE):
+        case(WM_DESTROY ):
+        case(WM_QUIT):{
+            global_running = 0;
+        }break;
         default:{
             result = DefWindowProc(window, msg, w_param, l_param);
         }break;
@@ -32,14 +49,54 @@ INT WINAPI WinMain(HINSTANCE instance,
     global_instance_handle = instance;
     
     WNDCLASSA window_class = {0};
+    
     {
         window_class.style = CS_HREDRAW | CS_VREDRAW;
         window_class.lpfnWndProc = w32_window_proc;
         window_class.hInstance = global_instance_handle;
         //window_class.hIcon = ;
         //window_class.hCursor = ;
+        window_class.hbrBackground = (HBRUSH)GetStockObject(DKGRAY_BRUSH);
         window_class.lpszClassName = "AppWindowClass";
     }
+
+
+    if(!RegisterClassA(&window_class)){
+        // TODO: error handling.
+        goto quit;
+    }
+
+    HWND window_handle = CreateWindowEx(0,
+                                        window_class.lpszClassName, 
+                                        "Ced",
+                                        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        CW_USEDEFAULT,
+                                        0, 0,
+                                        global_instance_handle,
+                                        0);
+
+    if(!window_handle){
+        // TODO: error handling.
+        goto quit;
+        
+    }
+
+
+    global_running = 1;
+    while(global_running){
+        
+        MSG message;
+        while(PeekMessage(&message, 0, 0, 0, PM_REMOVE)){
+
+            TranslateMessage(&message);
+            DispatchMessageA(&message);
+        }
+    }
+
+quit:;
     
     return(0);
 }
